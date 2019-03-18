c0be78233c1a90caa52e9e2a36ad195d078a82ea
Kuang Wei <kuangwei@uchicago.edu>
2019-03-17 23:58:06 -0500

    add bustimes.py

---
+++ bustimes.py
@@ -0,0 +1,19 @@
+from ctabustracker import CTABusTracker
+import config as cfg
+
+global busstop, cbt
+busstop = '1521'
+cbt = CTABusTracker(cfg.buskey)
+
+def printtime(cbt, busstop):
+	times = cbt.get_stop_predictions(busstop)
+	for time in times:
+		busnum = time['route_id']
+		arrivaltime = time['prediction']
+		currenttime = cbt.get_time()
+		waittime = arrivaltime - currenttime
+		print "%s is arriving at %s in %.0f minutes"%(busnum,
+													arrivaltime.strftime("%H:%M"),
+													waittime.seconds/60.)
+if __name__ == '__main__':
+	printtime(cbt, busstop)
