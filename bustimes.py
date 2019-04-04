c29a1a1b31daf17d20cf695b19c735d8c892c3ca
Kuang Wei <kuangwei@uchicago.edu>
2019-04-03 19:45:24 -0500

    interface with LCD

--- bustimes.py
+++ bustimes.py
@@ -1,5 +1,7 @@
 from ctabustracker import CTABusTracker
 import config as cfg
+import Adafruit_CharLCD as LCD
+import time
 
 global busstop, cbt
 busstop = '1521'
@@ -7,13 +9,51 @@ cbt = CTABusTracker(cfg.buskey)
 
 def printtime(cbt, busstop):
 	times = cbt.get_stop_predictions(busstop)
+	lcdmessage = ''
 	for time in times:
 		busnum = time['route_id']
 		arrivaltime = time['prediction']
 		currenttime = cbt.get_time()
 		waittime = arrivaltime - currenttime
-		print "%4s is arriving at %s in %2.0f minutes"%(busnum,
+		print("%4s is arriving at %s in %2.0f minutes"%(busnum,
 												arrivaltime.strftime("%H:%M"),
-												waittime.seconds/60.)
+												waittime.seconds/60.))
+		print("%-4s%2.0fmin %s"%(busnum, waittime.seconds/60.,
+								 arrivaltime.strftime("%H:%M")))
+		lcdmessage+="%-4s%2.0fmin %s\n"%(busnum, waittime.seconds/60.,    
+                             	   arrivaltime.strftime("%H:%M"))
+	#print(lcdmessage)
+	lcddisplay(lcdmessage)
+def lcddisplay(message):
+	lcd_rs        = 27
+	lcd_en        = 22
+	lcd_d4        = 25
+	lcd_d5        = 24
+	lcd_d6        = 23
+	lcd_d7        = 18
+	lcd_backlight = 4
+	lcd_columns = 16
+	lcd_rows    = 2
+
+	lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+	
+	lcd.message('Hello\nworld!')
+	lcd.clear()
+	lcd.show_cursor(True)
+	lcd.message('')
+	
+	lcd_rs        = 25
+	lcd_d4        = 23
+	lcd_d5        = 17
+	lcd_d6        = 21
+	lcd_d7        = 22
+	lcd_backlight = 16
+	lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+	
+	time.sleep(0.8)
+	lcd.message(message)
+
 if __name__ == '__main__':
 	printtime(cbt, busstop)
